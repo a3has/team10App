03ec90cfadf12822ae1c0d9d357d8d5a3688d108
Nikko Timbol <franznikko.timbol@sjsu.edu>
2023-11-18 13:04:41 -0800

    Fix again

--- notesapp/forms.py
+++ notesapp/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
 from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
 from models import User
 
