087acea8466ccbdf46db70895626206f0ae6c4f9
sammyboyking <sammy.cuaderno@sjsu.edu>
2023-11-26 19:56:42 -0800

    logging on to website works again

--- notesapp/app/routes.py
+++ notesapp/app/routes.py
@@ -7,14 +7,33 @@ from werkzeug.urls import url_parse
 from forms import RegistrationForm, AdvancedSearchForm, NoteForm2
 
 
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.check_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user, remember=form.remember_me.data)
+        next_page = request.args.get('next')
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('login')
+        return redirect(next_page)
+    return render_template('login.html', title='Sign In', form=form)
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home')
+@login_required
 def home():
     return render_template('home.html')
 
 
 @app.route('/notes', methods=['GET', 'POST'])
-@login_required
+
 def notes():
     form = NoteForm2()
     if form.validate_on_submit():
@@ -27,28 +46,12 @@ def notes():
     posts = current_user.get_notes()
     return render_template('notes.html', title='Home Page', form=form, posts=posts)
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('index'))
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = User.query.filter_by(username=form.username.data).first()
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        if not next_page or url_parse(next_page).netloc != '':
-            next_page = url_for('index')
-        return redirect(next_page)
-    return render_template('login.html', title='Sign In', form=form)
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if current_user.is_authenticated:
-        return redirect(url_for('index'))
+        return redirect(url_for('login'))
     form = RegistrationForm()
     if form.validate_on_submit():
         user = User(username=form.username.data, email=form.email.data)
@@ -127,7 +130,7 @@ def advanced_search():
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('login'))
 
 @app.route('/notes-nikko')
 def show_notes():
