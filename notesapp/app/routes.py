513e3ad72a404d78d30c0bd8bb90c654028e7b10
kiloGram <abed3hasbini@gmail.com>
2023-11-28 15:30:31 -0800

    added events from Google Calendar with basic Google calendar embed view

--- notesapp/app/routes.py
+++ notesapp/app/routes.py
@@ -1,11 +1,187 @@
-from flask import render_template, flash, redirect, url_for, request, abort
+from flask import render_template, flash, redirect, url_for, request, abort, session
 from app import app, db
 from forms import LoginForm, NoteForm
 from flask_login import current_user, login_user, logout_user, login_required
 from models import Note, User, Todo, NotePost
 from werkzeug.urls import url_parse
 from forms import RegistrationForm, AdvancedSearchForm, NoteForm2
+import google.auth
+from googleapiclient.discovery import build
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+from googleapiclient.errors import HttpError
+from google_auth_oauthlib.flow import InstalledAppFlow
+import os.path
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+import datetime
+import os
 
+import google.oauth2.credentials
+import google_auth_oauthlib.flow
+
+with app.app_context():
+
+    CLIENT_SECRETS_FILE = "app/client_secret.json"
+    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+    def credentials_to_dict(credentials):
+        return {'token': credentials.token,
+                'refresh_token': credentials.refresh_token,
+                'token_uri': credentials.token_uri,
+                'client_id': credentials.client_id,
+                'client_secret': credentials.client_secret,
+                'scopes': credentials.scopes}
+
+
+@app.route('/testlogin')
+def index():
+    return print_index_table()
+
+
+@app.route('/Googlelogin')
+def Googlelogin():
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES)
+
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
+    authorization_url, state = flow.authorization_url(
+        access_type='offline', include_granted_scopes='true')
+    session['state'] = state
+    return redirect(authorization_url)
+
+
+@app.route('/oauth2callback')
+def oauth2callback():
+
+    state = session['state']
+
+    flow = google_auth_oauthlib.flow.Flow.from_client_secrets_file(
+        CLIENT_SECRETS_FILE, scopes=SCOPES, state=state)
+    flow.redirect_uri = url_for('oauth2callback', _external=True)
+
+    # Use the authorization server's response to fetch the OAuth 2.0 tokens.
+    authorization_response = request.url
+    flow.fetch_token(authorization_response=authorization_response)
+
+    credentials = flow.credentials
+    session['credentials'] = credentials_to_dict(credentials)
+
+    return redirect('/calendar')
+    # return redirect(url_for('Googlelogin'))
+    # return redirect(url_for('calendarBasic'))
+    # return redirect('/calendarBasic')
+    # return redirect(url_for('testlogin'))
+    # return redirect('/testlogin')
+
+
+@app.route('/revoke')
+def revoke():
+    if 'credentials' not in session:
+        return ('You need to <a href="/authorize">authorize</a> before ' +
+                'testing the code to revoke credentials.')
+
+    credentials = google.oauth2.credentials.Credentials(
+        **session['credentials'])
+
+    revoke = request.post('https://oauth2.googleapis.com/revoke',
+                          params={'token': credentials.token},
+                          headers={'content-type': 'application/x-www-form-urlencoded'})
+
+    status_code = getattr(revoke, 'status_code')
+    if status_code == 200:
+        return ('Credentials successfully revoked.' + print_index_table())
+    else:
+        return ('An error occurred.' + print_index_table())
+
+
+def print_index_table():
+    return ('<table>' +
+            '<tr><td><a href="/test">Test an API request</a></td>' +
+            '<td>Submit an API request and see a formatted JSON response. ' +
+            '    Go through the authorization flow if there are no stored ' +
+            '    credentials for the user.</td></tr>' +
+            '<tr><td><a href="/authorize">Test the auth flow directly</a></td>' +
+            '<td>Go directly to the authorization flow. If there are stored ' +
+            '    credentials, you still might not be prompted to reauthorize ' +
+            '    the application.</td></tr>' +
+            '<tr><td><a href="/revoke">Revoke current credentials</a></td>' +
+            '<td>Revoke the access token associated with the current user ' +
+            '    session. After revoking credentials, if you go to the test ' +
+            '    page, you should see an <code>invalid_grant</code> error.' +
+            '</td></tr>' +
+            '<tr><td><a href="/clear">Clear Flask session credentials</a></td>' +
+            '<td>Clear the access token currently stored in the user session. ' +
+            '    After clearing the token, if you <a href="/test">test the ' +
+            '    API request</a> again, you should go back to the auth flow.' +
+            '</td></tr></table>')
+
+
+@app.route('/clear')
+def clear_credentials():
+    if 'credentials' in session:
+        del session['credentials']
+    return ('Credentials have been cleared.<br><br>' +
+            print_index_table())
+
+
+@app.route('/calendar')
+def calendar():
+    creds = None
+
+    if os.path.exists("token.json"):
+        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "app/client_secret.json", SCOPES
+            )
+            creds = flow.run_local_server(port=8080)
+    # Save the credentials for the next run
+        with open("token.json", "w") as token:
+            token.write(creds.to_json())
+
+    try:
+        service = build("calendar", "v3", credentials=creds)
+
+        # Call the Calendar API
+        now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+        print("Getting the upcoming 10 events")
+        events_result = (
+            service.events()
+            .list(
+                calendarId="primary",
+                timeMin=now,
+                maxResults=10,
+                singleEvents=True,
+                orderBy="startTime",
+            )
+            .execute()
+        )
+        events = events_result.get("items", [])
+
+        if not events:
+            print("No upcoming events found.")
+            return
+
+        # Prints the start and name of the next 10 events
+        formatted_events = []
+        for event in events:
+            start = event["start"].get("dateTime", event["start"].get("date"))
+            formatted_events.append((start, event["summary"]))
+
+    except HttpError as error:
+        print(f"An error occurred: {error}")
+
+    return render_template('calendar.html', results=formatted_events)
+
+
+@app.route('/calendarBasic', methods=['GET', 'POST'])
+def Gcal():
+    return render_template('calendarBasic.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -25,6 +201,7 @@ def login():
         return redirect(next_page)
     return render_template('login.html', title='Sign In', form=form)
 
+
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home')
 @login_required
@@ -33,11 +210,11 @@ def home():
 
 
 @app.route('/notes', methods=['GET', 'POST'])
-
 def notes():
     form = NoteForm2()
     if form.validate_on_submit():
-        note = NotePost(title=form.title.data, body=form.note.data, author=current_user)
+        note = NotePost(title=form.title.data,
+                        body=form.note.data, author=current_user)
         db.session.add(note)
         db.session.commit()
         return redirect(url_for('notes'))
@@ -45,7 +222,6 @@ def notes():
         print(form.errors)
     posts = current_user.get_notes()
     return render_template('notes.html', title='Home Page', form=form, posts=posts)
-
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -62,6 +238,7 @@ def register():
         return redirect(url_for('login'))
     return render_template('register.html', title='Register', form=form)
 
+
 @app.route('/profile', methods=['GET', 'POST'])
 def edit_profile():
     if request.method == 'POST':
@@ -75,35 +252,39 @@ def edit_profile():
 
     return render_template('userprofile.html')
 
+
 @app.route('/todo')
 def todo():
     todo_list = Todo.query.filter_by(user_id=current_user.id).all()
-    #todo_list = current_user.Todo.all()
-    #todo_list=Todo.query.all()
-    return render_template('todo.html',todo_list=todo_list)
+    # todo_list = current_user.Todo.all()
+    # todo_list=Todo.query.all()
+    return render_template('todo.html', todo_list=todo_list)
 
-@app.route('/add',methods=['POST'])
+
+@app.route('/add', methods=['POST'])
 def add():
     name = request.form.get("name")
-    new_task=Todo(name=name,done=False,user_id=current_user.id)
+    new_task = Todo(name=name, done=False, user_id=current_user.id)
     db.session.add(new_task)
     db.session.commit()
     return redirect(url_for("todo"))
 
+
 @app.route('/update/<int:todo_id>')
 def update(todo_id):
-    todo= Todo.query.get(todo_id)
-    todo.done=not todo.done
+    todo = Todo.query.get(todo_id)
+    todo.done = not todo.done
     db.session.commit()
     return redirect(url_for("todo"))
 
 
 @app.route('/delete/<int:todo_id>')
 def delete(todo_id):
-    todo= Todo.query.get(todo_id)
+    todo = Todo.query.get(todo_id)
     db.session.delete(todo)
     db.session.commit()
     return redirect(url_for("todo"))
+
 
 @app.route('/advanced_searching', methods=['GET', 'POST'])
 def advanced_search():
@@ -112,7 +293,7 @@ def advanced_search():
     if form.validate_on_submit():
         # Build the query based on form input
         query = Todo.query.filter_by(user_id=current_user.id)
-        #query = Todo.query
+        # query = Todo.query
 
         if form.task_name.data:
             query = query.filter(Todo.name.ilike(f'%{form.task_name.data}%'))
@@ -120,22 +301,25 @@ def advanced_search():
         if form.is_complete.data is not None:
             query = query.filter(Todo.done == form.is_complete.data)
 
-        # Execute the query for all 
+        # Execute the query for all
         results = query.all()
 
         return render_template('search_results.html', results=results)
 
     return render_template('adv_search.html', form=form)
 
+
 @app.route('/logout')
 def logout():
     logout_user()
     return redirect(url_for('login'))
 
+
 @app.route('/notes-nikko')
 def show_notes():
     notes = Note.query.filter_by(user_id=current_user.id).all()
     return render_template('notes.html', notes=notes)
+
 
 @app.route('/create_note', methods=['GET', 'POST'])
 def create_note():
@@ -143,13 +327,14 @@ def create_note():
 
     if form.validate_on_submit():
         content = form.content.data
-        new_note = Note(content=content,user_id=current_user.id)
+        new_note = Note(content=content, user_id=current_user.id)
         db.session.add(new_note)
         db.session.commit()
         flash('Note added successfully!', 'success')
         return redirect(url_for('show_notes'))
 
     return render_template('create_note.html', form=form)
+
 
 @app.route('/edit_note/<int:note_id>', methods=['GET', 'POST'])
 @login_required
@@ -167,6 +352,7 @@ def edit_note(note_id):
         form.title.data = note.title
         form.note.data = note.body
     return render_template('edit_note.html', title='Edit Note', form=form, note_id=note.id)
+
 
 @app.route('/delete_note/<int:note_id>', methods=['POST'])
 @login_required
