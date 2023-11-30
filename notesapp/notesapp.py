257ca779d791618dc551253e78ca49f6b0385a25
sammyboyking <sammy.cuaderno@sjsu.edu>
2023-11-29 22:38:31 -0800

    edited more

--- notesapp/notesapp.py
+++ notesapp/notesapp.py
@@ -2,11 +2,12 @@ from app import app, db
 from models import User, Note
 import os
 
+#set environmental variables for developers 
 os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
 os.environ['OAUTHLIB_RELAX_TOKEN_SCOPE'] = '1'
 os.environ['WTF_CSRF_CHECK_DEFAULT'] = 'False'
 
 
 @app.shell_context_processor 
-def make_shell_context():
+def make_shell_context(): # define a shell 
     return {'db': db, 'User': User, 'Note': Note}
